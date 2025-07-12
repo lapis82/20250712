f770dd3fe57ceb04805f14c8a7ac065c061ce248
lapis82 <classica@snu.ac.kr>
2025-07-12 16:54:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ st.title('sj first webapp')
 name=st.text_input ('name:')
 menu=st.selectbox('favourite menu:', ['chocolate', 'lemonade', 'tea'])
 if st.button('welcome'):
-  st.write( 'Hello, ' +name+ '! your favourite menu is ' +menu+ '. it is fine!')
+  st.write( 'Hello, ' +name+ '! Your favourite menu is ' +menu+ '. it is fine!')
